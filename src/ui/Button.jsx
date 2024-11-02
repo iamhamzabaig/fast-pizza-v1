fa2572f30a54c27c23d54f345f762b102d66ca18
Hamza Baig <hamzamunawar345@gmail.com>
2024-11-02 22:23:08 +0500

    Design changes of tailwind css of Buttons $ Input CSS

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,9 +1,16 @@
-export default function Button({ children, disbled }) {
+import { Link } from "react-router-dom";
+
+export default function Button({ children, disbled, to }) {
+  const className =
+    "inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-1 disabled:cursor-not-allowed sm:px-6 sm:py-4";
+  if (to)
+    return (
+      <Link className={className} to={to}>
+        {children}
+      </Link>
+    );
   return (
-    <button
-      disabled={disbled}
-      className="inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-1 disabled:cursor-not-allowed sm:px-6 sm:py-4"
-    >
+    <button disabled={disbled} className={className}>
       {children}
     </button>
   );
